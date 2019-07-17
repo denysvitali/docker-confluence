53ba71a4b4246a2cc3781b76c90879407366ed48
Denys Vitali <denys@denv.it>
2019-07-17 11:57:06 +0200

    Do not perform the initial configuration twice

--- entrypoint.sh
+++ entrypoint.sh
@@ -25,32 +25,38 @@ if [ -n "${CONFLUENCE_PROXY_SCHEME}" ]; then
   xmlstarlet ed -P -S -L --insert "//Connector[not(@scheme)]" --type attr -n scheme --value "${CONFLUENCE_PROXY_SCHEME}" ${CONFLUENCE_INSTALL}/conf/server.xml
 fi
 
-if [ -n "$CONFLUENCE_ATLASSIAN_LICENSE_MESSAGE" ]; then
-  add_prop "atlassian.license.message" "$CONFLUENCE_ATLASSIAN_LICENSE_MESSAGE"
+if [ ! -f $CONFLUENCE_HOME/.container-config-ok ]; then
+  if [ -n "$CONFLUENCE_ATLASSIAN_LICENSE_MESSAGE" ]; then
+    add_prop "atlassian.license.message" "$CONFLUENCE_ATLASSIAN_LICENSE_MESSAGE"
+  fi
+  
+  if [ -n "$CONFLUENCE_SERVER_ID" ]; then
+    add_prop "confluence.setup.server.id" "$CONFLUENCE_SERVER_ID"
+  fi
+  
+  if [ -n "$CONFLUENCE_DB_URL" ]; then
+    add_prop "hibernate.connection.url" "$CONFLUENCE_DB_URL"
+  fi
+  
+  if [ -n "$CONFLUENCE_DB_USERNAME" ]; then
+    add_prop "hibernate.connection.username" "$CONFLUENCE_DB_USERNAME"
+  fi
+  
+  if [ -n "$CONFLUENCE_DB_PASSWORD" ]; then
+    add_prop "hibernate.connection.password" "$CONFLUENCE_DB_PASSWORD"
+  fi
+  
+  if [ -n "$CONFLUENCE_DB_DRIVER_CLASS" ]; then
+    add_prop "hibernate.connection.driver_class" "$CONFLUENCE_DB_DRIVER_CLASS"
+  fi
+  
+  if [ -n "$CONFLUENCE_DB_DIALECT" ]; then
+    add_prop "hibernate.dialect" "$CONFLUENCE_DB_DIALECT"
+  fi
+
+  touch $CONFLUENCE_HOME/.container-config-ok;
 fi
 
-if [ -n "$CONFLUENCE_SERVER_ID" ]; then
-  add_prop "confluence.setup.server.id" "$CONFLUENCE_SERVER_ID"
-fi
-
-if [ -n "$CONFLUENCE_DB_URL" ]; then
-  add_prop "hibernate.connection.url" "$CONFLUENCE_DB_URL"
-fi
-
-if [ -n "$CONFLUENCE_DB_USERNAME" ]; then
-  add_prop "hibernate.connection.username" "$CONFLUENCE_DB_USERNAME"
-fi
-
-if [ -n "$CONFLUENCE_DB_PASSWORD" ]; then
-  add_prop "hibernate.connection.password" "$CONFLUENCE_DB_PASSWORD"
-fi
-
-if [ -n "$CONFLUENCE_DB_DRIVER_CLASS" ]; then
-  add_prop "hibernate.connection.driver_class" "$CONFLUENCE_DB_DRIVER_CLASS"
-fi
-
-if [ -n "$CONFLUENCE_DB_DIALECT" ]; then
-  add_prop "hibernate.dialect" "$CONFLUENCE_DB_DIALECT"
-fi
 echo "Launching Confluence..."
+
 /opt/confluence-scripts/launch.sh $CONFLUENCE_INSTALL $CONFLUENCE_HOME
